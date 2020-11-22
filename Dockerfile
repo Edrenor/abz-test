c42adb24e23fa899b0fdd0b283877d9810ae320e
alexander.kalinin <phex13@mail.ru>
2020-11-22 15:25:34 +0300

    dockerfile settings added, migrations

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
 FROM php:7.3-fpm
-RUN apt-get update -y && apt-get install -y libmcrypt-dev openssl libpq-dev
+COPY composer.lock composer.json /var/www/
 
-RUN docker-php-ext-install pdo pdo_pgsql mbstring
+RUN apt-get update -y && apt-get install -y libmcrypt-dev openssl libpq-dev libfreetype6-dev libpng-dev libjpeg62-turbo-dev jpegoptim optipng pngquant gifsicle libzip-dev
+
+RUN docker-php-ext-install pdo pdo_mysql zip exif pcntl
+RUN docker-php-ext-configure gd --with-gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ --with-png-dir=/usr/include/
+RUN docker-php-ext-install gd
+
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-RUN docker-php-ext-install pdo pdo_pgsql mbstring
+RUN docker-php-ext-install pdo pdo_mysql
 WORKDIR /app
 COPY . /app
 RUN composer install
--- database/migrations/2020_11_22_104634_create_employees_table.php
+++ database/migrations/2020_11_22_104634_create_employees_table.php
@@ -15,7 +15,22 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->unsignedBigInteger('position_id');
+            $table->date('employment_date');
+            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->integer('salary');
+            $table->unsignedBigInteger('photo_id');
+            $table->unsignedBigInteger('admin_created_at_id');
+            $table->unsignedBigInteger('admin_updated_at_id');
             $table->timestamps();
+
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->foreign('photo_id')->references('id')->on('photos');
+            $table->foreign('admin_created_at_id')->references('id')->on('users');
+            $table->foreign('admin_updated_at_id')->references('id')->on('users');
+
         });
     }
 
